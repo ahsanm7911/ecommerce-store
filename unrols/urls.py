d9250b50f2fdb91847bc572cc7657934e97885ad
ahsan mohammad <ahsanm7911@gmail.com>
2023-06-05 15:20:18 +0500

    static settings updated

--- unrols/urls.py
+++ unrols/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin7911/', admin.site.urls),
     path('', include('core.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
