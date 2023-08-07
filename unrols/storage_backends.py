ea881c08f3b3469da822df481ce5bc3b4d4445ff
ahsan mohammad <ahsanm7911@gmail.com>
2023-08-07 12:25:29 +0500

    media folder added

--- unrols/storage_backends.py
+++ unrols/storage_backends.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3StaticStorage):
+class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
 
