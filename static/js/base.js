1e62bafadcc56847c252836643750f1f09b46a25
ahsan mohammad <ahsanm7911@gmail.com>
2023-08-21 13:14:14 +0500

    static updated

--- static/js/base.js
+++ static/js/base.js
@@ -4,3 +4,20 @@ function updateCartData() {
     let cart_length = Object.keys(cart).length
     cartCountEle.text(String(cart_length))
 }
+
+$(document).ready(function(){
+    let messageDiv = $('.message-div').hide()
+    $('#newsletter').submit(function(e){
+        e.preventDefault()
+        let formData = $(this).serialize()
+        $.ajax({
+            url: '/newsletter/',
+            type: 'POST',
+            data: formData,
+            success: function(response){
+                messageDiv.children('span').text(response)
+                messageDiv.show(600)
+            }
+        })
+    })
+})
