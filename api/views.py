2fc78fd4e6e6c5fbd25fdabe3ed5802e8c511e48
Ahsan Mohammad <ahsanm7911@gmail.com>
2023-09-20 22:51:52 +0500

    error handling

--- api/views.py
+++ api/views.py
@@ -14,26 +14,29 @@ def ig_feed(request):
 
 @api_view(['GET'])
 def display_orders(request):
-    orders = ConfirmedOrder.objects.all()
-    all_orders = []
-    for order in orders:
-        order_data = {}
-        full_name = order.shippingAddress.split('\n')[0]
-        name = full_name.split(' ')[0] + ' ' + full_name.split(' ')[1][0].capitalize() + '.'
-        item = order.order_details.split('\n')[0].split('|')[0].split(' x ')[0].strip()
-        color = order.order_details.split('\n')[0].split('|')[1].split(':')[1].strip()
-        print(item)
-        print(color)
-        try:
-            item_image = ProductVariant.objects.get(product__name=item, color__name=color).thumbnail
-        except ProductVariant.DoesNotExist:
-            item_image = ''
-        order_data = {
-            'name': name,
-            'item': item,
-            'image': item_image
-        }
-        all_orders.append(order_data)
-    return Response(all_orders)
+    try:
+        orders = ConfirmedOrder.objects.all()
+        all_orders = []
+        for order in orders:
+            order_data = {}
+            full_name = order.shippingAddress.split('\n')[0]
+            name = full_name.split(' ')[0] + ' ' + full_name.split(' ')[1][0].capitalize() + '.'
+            item = order.order_details.split('\n')[0].split('|')[0].split(' x ')[0].strip()
+            color = order.order_details.split('\n')[0].split('|')[1].split(':')[1].strip()
+            print(item)
+            print(color)
+            try:
+                item_image = ProductVariant.objects.get(product__name=item, color__name=color).thumbnail
+            except ProductVariant.DoesNotExist:
+                item_image = ''
+            order_data = {
+                'name': name,
+                'item': item,
+                'image': item_image
+            }
+            all_orders.append(order_data)
+        return Response(all_orders)
+    except Exception as e:
+        print(e)
 
 
